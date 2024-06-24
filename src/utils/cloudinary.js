b7f0e68d6262877a753ea790307a3f803b903d6b
jangidRkt08 <itsmejangid.rk@gmail.com>
2024-06-24 20:03:51 +0530

    uploading files to backend

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,40 @@
+import { v2 as cloudinary } from "cloudinary";
+import { log } from "console";
+import { response } from "express";
+import fs from "fs";
+
+// Configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadonCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    //  uplaod file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resourse_type: "auto",
+    });
+    // file has been successfully upladed
+    console.log("file is uploaded in cloudinary", response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); //remove locally seaved temporary file as the uplaod operation failed
+    return null;
+  }
+};
+
+// Upload an image
+const uploadResult = await cloudinary.uploader
+  .upload(
+    "https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg",
+    {
+      public_id: "shoes",
+    }
+  )
+  .catch((error) => {
+    console.log(error);
+  });
+export { uploadonCloudinary };
